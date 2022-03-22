624c2d0ab1a56af7977212815a8b3534e351517b
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-03-23 01:34:25 +0430

    implement process/action.php for handling form data

---
+++ process/action.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+$_SESSION = null;
+$msg = null;
+
+try {
+
+	if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+		$msg = "invalid request try again";
+	}
+
+	if (!isset($_POST["send"]) || empty($_POST["send"]) || $_POST["send"] != "send") {
+		$msg = "invalid request try again";
+	}
+
+	if (!isset($_POST["number"]) || empty($_POST["number"])) {
+		$rand_num = null;
+	}
+
+	$number = filter_var($_POST["number"], FILTER_SANITIZE_NUMBER_INT);
+
+	$number = intval($number);
+
+	if (!is_numeric($number)) {
+		$msg = "the input is not valid try again only accept the number between 1 and 500";
+	}
+
+	$options = [
+		"options" => [
+			"min_range" => 1,
+			"max_range" => 500,
+			"cleanData"
+		]
+	];
+
+	if (
+		filter_var($number, FILTER_VALIDATE_INT, $options) === 0 ||
+		!filter_var($number, FILTER_VALIDATE_INT, $options) === false
+	) {
+		$msg = null;
+	} else {
+		$msg = "your input is not valid or not in the range";
+	}
+
+	$rand_num = mt_rand(1, 500);
+
+	if($rand_num == $number) {
+		$_SESSION["msg"]["result"] = TRUE;
+	}
+
+	$_SESSION["msg"]["error"] = $msg;
+	$_SESSION["numbers"]["rand_num"] = $rand_num;
+	$_SESSION["numbers"]["number"] = $number;
+
+	header("Location: ../index.php");
+} catch (exception $e) {
+	echo $e->getMessage();
+}
